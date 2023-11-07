2a8ab5b81fc463ba6c5e676693bb2029e8166d4c
thaihunghung <110120031@st.tvu.edu.vn>
2023-11-08 06:18:55 +0700

    handlebars and export html-pdf

--- scr/app.js
+++ scr/app.js
@@ -1,11 +1,34 @@
-const express = require('express')
-const app = express()
+const path = require('path');
+const fs = require('fs');
+const pdf = require('html-pdf');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const app = express();
+const handlebars = require('handlebars');
 const dotenv = require('dotenv')
 //khoi tao dotenv
 dotenv.config();
 
+
+//su dung templates hbs
+app.use(express.urlencoded({ extended: true }));
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: '.hbs',
+  partialsDir: path.join(__dirname, '/views/partials'),
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, './views'));
+
+
+
+
+
+
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.render('home')
 })
 
 const port = process.env.PORT || 8080;
