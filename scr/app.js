613607202139ff102483fc44dac346d7bd9a30a2
thaihunghung <110120031@st.tvu.edu.vn>
2023-11-10 11:51:11 +0700

    update dont use html-pdf to export pdf

--- scr/app.js
+++ scr/app.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const fs = require('fs');
-const pdf = require('html-pdf');
+const puppeteer = require('puppeteer');
 const express = require('express');
 const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
@@ -41,10 +41,54 @@ app.use('/monhoc', monHocRoutes);
 
 
 
-app.post('/updateA4Content', (req, res) => {
+app.all('/pdf', (req, res) => {
   const a4Content = req.body.a4Content;
-  console.log('Nội dung từ phần tử a4:', a4Content);
-  res.send('Nội dung từ phần tử a4 đã được cập nhật thành công!');
+  console.log(a4Content)
+  // Đọc nội dung CSS từ file
+  const cssContent = fs.readFileSync('./public/css/print/print.css', 'utf-8');
+  (async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    htmlContent = `<html><head><style>${cssContent}</style></head><body>${a4Content}</body></html>`;
+    
+    await page.setContent(htmlContent);
+    const pdfOptions = {
+      path: 'output.pdf',
+      format: 'A4',
+      margin: {
+        top: '2cm',
+        right: '2cm',
+        bottom: '2cm',
+        left: '3cm',
+      },
+    };
+  
+    // Generate the PDF with specified margins
+    await page.pdf(pdfOptions);
+  
+    await browser.close();
+  })();
+  // // Đặt tùy chọn cho html-pdf
+  // const options = {
+  //   format: 'A4',
+  //   border: {
+  //     top: '2cm',
+  //     left: '3cm',
+  //     right: '2cm',
+  //     bottom: '2cm',
+  //   },
+  // };
+
+  // // Chuyển đổi HTML thành PDF và ghi vào file
+  // pdf.create(htmlContent1, options).toFile('./output.pdf', (err, _) => {
+  //   if (err) {
+  //     console.error('Lỗi khi tạo PDF:', err);
+  //     res.status(500).send('Có lỗi xảy ra khi tạo PDF.');
+  //   } else {
+  //     console.log('PDF đã được tạo thành công');
+  //     res.send('Nội dung từ phần tử a4 đã được cập nhật và xuất PDF thành công!');
+  //   }
+  // });
 });
 
 
